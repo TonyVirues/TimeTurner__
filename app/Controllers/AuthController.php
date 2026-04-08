cc59918669e3b87365a66320a6960983ab24d724
“Mar8787” <mss7@outlook.com>
2026-04-08 21:41:07 +0200

    Arreglo estilos login, separado de custom

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -6,32 +6,33 @@ use App\Models\UserModel;
 
 class AuthController extends BaseController
 {
-    public function index()
-    {
-        return view('auth/login');
-    }
-    public function pie(){
-        return view('layaouts/footer');
-    }
-//     public function login()
-//     {
-//         $usuario = $this->request->getPost('usuario');
-//         $password = $this->request->getPost('password');
+  public function index()
+  {
+    return view('auth/login');
+  }
+  public function pie()
+  {
+    return view('layouts/footer');
+  }
+  //     public function login()
+  //     {
+  //         $usuario = $this->request->getPost('usuario');
+  //         $password = $this->request->getPost('password');
 
-//         $model = new UserModel();
-//         $user = $model->where('usuario', $usuario)->first();
+  //         $model = new UserModel();
+  //         $user = $model->where('usuario', $usuario)->first();
 
-//         if ($user && password_verify($password, $user['password'])) {
+  //         if ($user && password_verify($password, $user['password'])) {
 
-//             session()->set([
-//                 'id' => $user['id'],
-//                 'usuario' => $user['usuario'],
-//                 'logged_in' => true
-//             ]);
+  //             session()->set([
+  //                 'id' => $user['id'],
+  //                 'usuario' => $user['usuario'],
+  //                 'logged_in' => true
+  //             ]);
 
-//             return redirect()->to('/dashboard');
-//         }
+  //             return redirect()->to('/dashboard');
+  //         }
 
-//         return redirect()->back()->with('error', 'Credenciales incorrectas');
-//     }
+  //         return redirect()->back()->with('error', 'Credenciales incorrectas');
+  //     }
 }

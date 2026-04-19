728dc56a7c2bb2582dea973820790e1c53d68ffb
Tonyvirues <tonimavir@gmail.com>
2026-04-19 08:15:09 +0200

    insercción de ususarios en db

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -15,11 +15,6 @@
     <!-- Topbar -->
     <header class="d-flex align-items-center gap-3 px-4 bg-white border-bottom" style="height: 60px; min-height: 60px;">
 
-      <!-- Toggle sidebar -->
-      <button class="btn btn-light btn-sm tt-toggle-btn" id="ttToggleBtn" title="Menú">
-        <span class="material-symbols-outlined">menu</span>
-      </button>
-
       <!-- Título -->
       <div class="flex-grow-1">
         <span class="fw-bold tt-topbar-title">TimeTurner</span>
